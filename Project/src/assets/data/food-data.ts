2804ffa815af0fa75113a03e3ea9f5dbc91bbbf8
ayamarmash <ayamarmash74@gmail.com>
2023-02-28 14:15:57 +0200

    organize toolbal files

--- Project/src/assets/data/food-data.ts
+++ Project/src/assets/data/food-data.ts
@@ -23,26 +23,5 @@ export const FoodData : Food[] = [
     FoodPrice: "50",
     FoodImg :"../../../assets/images/burger.svg",
     Type: "Dessert"
-  },
-  {
-    FoodName: "Peanut Butter",
-    Description: Description,
-    FoodPrice: "45",
-    FoodImg :"../../../assets/images/chicken.svg",
-    Type: "Sandwich"
-  },
-  {
-    FoodName: "Pumpkin Pie",
-    Description: Description,
-    FoodPrice: "35",
-    FoodImg :"../../../assets/images/chicken2.svg",
-    Type: "Dessert"
-  },
-  {
-    FoodName: "Jambalaya",
-    Description: Description,
-    FoodPrice: "50",
-    FoodImg :"../../../assets/images/burger.svg",
-    Type: "Dessert"
   }
 ];
