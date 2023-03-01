bdc44a47e0f5a2c9db5bb48d5d269e3d0a7074ec
ayamarmash <ayamarmash74@gmail.com>
2023-03-01 04:10:28 +0200

    edits

--- Project/src/app/components/food-card/food-card.component.ts
+++ Project/src/app/components/food-card/food-card.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input} from '@angular/core';
+import {DataService} from "../../services/data-service/data.service";
+import {AddToCartFavService} from "../../services/add-to-cart-favorite-service/add-to-cart-favorite.service";
 
 @Component({
   selector: 'app-food-card',
@@ -6,9 +8,18 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./food-card.component.scss']
 })
 export class FoodCardComponent {
+  constructor(private addToCartService : AddToCartFavService) {}
   @Input() foodName : string = "Food"
   @Input() Description : string = ""
   @Input() foodPrice : string = "0"
   @Input() foodImg : string = "../../../assets/images/chicken.svg"
   @Input() left : boolean = false
+
+  addToCart(){
+    this.addToCartService.addToCart({
+      foodName: this.foodName,
+      foodPrice: this.foodPrice,
+      foodImg: this.foodImg
+    })
+  }
 }
